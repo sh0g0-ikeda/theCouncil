d9f1f7e4c379aaacd994b5b5555bfa4e009b5290
shogo <shogo@example.com>
2026-03-14 02:02:31 +0900

    fix: disable typedRoutes

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  experimental: {
-    typedRoutes: true
-  }
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
 
