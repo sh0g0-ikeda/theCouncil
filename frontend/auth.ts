b15f0907b4a5284d9c097fa776a078d0636b974c
shogo <shogo@example.com>
2026-03-14 02:04:46 +0900

    fix: remove version from TwitterProvider

--- frontend/auth.ts
+++ frontend/auth.ts
@@ -22,8 +22,7 @@ if (process.env.TWITTER_CLIENT_ID && process.env.TWITTER_CLIENT_SECRET) {
   providers.push(
     TwitterProvider({
       clientId: process.env.TWITTER_CLIENT_ID,
-      clientSecret: process.env.TWITTER_CLIENT_SECRET,
-      version: "2.0"
+      clientSecret: process.env.TWITTER_CLIENT_SECRET
     })
   );
 }
